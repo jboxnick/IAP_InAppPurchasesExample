f2380fe6c92a7d12981dbc5b5acbcf48dd2334fb
jboxnick <j.boxnick@web.de>
2022-03-20 00:24:59 +0100

    Initial Commit

---
+++ Animals IAP Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Animals IAP Example
+//
+//  Created by Julian on 20.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
